aac057c67ca6bbb69af6b5a0f5860b768cc1e4e1
Samuel Fagundes <picantepuppet@gmail.com>
2022-03-05 23:26:49 -0300

    pong-1

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local push = require 'push'
+local push = require 'libraries/push'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -8,6 +8,10 @@ VIRTUAL_HEIGHT = 243
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    local smallFont = love.graphics.newFont('/fonts/font.ttf', 8)
+
+    love.graphics.setFont(smallFont)
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -25,13 +29,21 @@ end
 function love.draw()
     push:apply('start')
 
+    love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+
     love.graphics.printf(
         'Hello Pong!',
         0,
-        VIRTUAL_HEIGHT / 2 - 6,
+        VIRTUAL_HEIGHT / 2 - 16,
         VIRTUAL_WIDTH,
         'center'
     )
-    
+
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
     push:apply('end')
 end
